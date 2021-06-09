684fac4a028c7d802861b72b00d951314d44d94e
Corey <corey.amano@gmail.com>
2021-06-08 17:44:43 -0700

    set up single options hash

--- class_practice.rb
+++ class_practice.rb
@@ -2,14 +2,17 @@
 #a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
 #b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
 
+#Rewrite your store items using a class with a single options hash in the initialize method.
+
+
 #create class and def initialize
 
 class Coffee
 
-  def initialize(input_item_name, input_price, input_availability)
-    @item_name = input_item_name
-    @price = input_price
-    @availability = input_availability
+  def initialize(input_options)
+    @item_name = input_options[:item_name]
+    @price = input_options[:price]
+    @availability = input_options[:availability]
   end
 
   def coffee_info
@@ -40,9 +43,9 @@ class Coffee
 
 end
 
-coffee1 = Coffee.new("hot coffee", 3.00, true)
-coffee2 = Coffee.new("cold brew", 4.00, true)
-coffee3 = Coffee.new("decaf coffee", 2.50, false)
+coffee1 = Coffee.new({:item_name => "hot coffee", :price => 3.00, :availability => true})
+coffee2 = Coffee.new(item_name: "cold brew", price: 4.00, availability: true)
+coffee3 = Coffee.new(item_name: "decaf coffee", availability: false, price: 2.50)
 
 coffee1.coffee_info
 coffee1.price=(3.50)
@@ -53,3 +56,7 @@ coffee1.coffee_info
 p "#{coffee2.item_name}"
 p "#{coffee2.price}"
 p "#{coffee2.availability}"
+
+p "#{coffee3.item_name}"
+p "#{coffee3.price}"
+p "#{coffee3.availability}"
